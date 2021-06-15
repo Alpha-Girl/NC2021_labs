e370fd8a9a12dfa12a6b021c4c5ca2af7153e234
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2021-06-15 09:36:47 +0800

    save

---
+++ lab3/src/hw3_1_1.m
@@ -0,0 +1,16 @@
+clear,clc
+
+syms x;
+F = @(x) sin(x);
+result = [0:15];
+h_l = result;
+tmp = cos(1.2);
+f = F(1.2);
+
+for i = 0:15
+    h = 10^(-i);
+    h_l(i + 1) = h;
+    result(i + 1) = abs((F(1.2 + i) - f) / h - tmp);
+end
+
+loglog(h_l, result);
